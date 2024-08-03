fd466b63945de203444e6aad8847a10c5316297f
Seerat <gurseerat0102@gmail.com>
2024-08-02 20:34:25 -0700

    code

---
+++ out/production/FeedForwardApplication.java
@@ -0,0 +1,17 @@
+package com.seerat.feedforward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedForwardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedForwardApplication.class, args);
+         try {
+            System.out.print("hi");
+        } catch (Exception e) {
+           throw new RuntimeException(e);
+         }
+    }
+}
